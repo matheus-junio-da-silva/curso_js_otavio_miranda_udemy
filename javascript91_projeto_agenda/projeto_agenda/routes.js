8112aade5ef7fe0b06eeafbdd62e8ca1581d5b70
Matheus Júnio <100482426+matheus-junio-da-silva@users.noreply.github.com>
2023-02-01 23:19:23 -0300

    pagina de contato

--- javascript91_projeto_agenda/projeto_agenda/routes.js
+++ javascript91_projeto_agenda/projeto_agenda/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+const { loginRequired } = require('./src/middlewares/middleware');
 // rotas da home
 route.get('/', homeController.index);
 
@@ -10,5 +12,8 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// rotas de contato
+route.get('/contato/index', loginRequired , contatoController.index);
 
 module.exports = route;
